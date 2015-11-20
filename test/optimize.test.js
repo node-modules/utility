4c29bb538f869cb1e3613f089c596cf956dd313a
alsotang <alsotang@gmail.com>
2015-11-20 11:18:31 +0800

    utility.dig

--- test/optimize.test.js
+++ test/optimize.test.js
@@ -47,4 +47,26 @@ describe('optimize.test.js', function () {
       should.not.exist(res.value);
     });
   });
+
+  describe('dig()', function () {
+    it('should work with {}', function () {
+      utils.dig({}).should.eql({});
+    });
+
+    it('should work with undefined', function () {
+      should(utils.dig() === void 0).ok();
+    });
+
+    it('should work with {a: 1}', function () {
+      utils.dig({a: 1}).should.eql({a: 1});
+    });
+
+    it('should work with {a: 1} when access `a`', function () {
+      utils.dig({a: 1}, 'a').should.eql(1);
+    });
+
+    it('should work with {a: 1} when access deep key', function () {
+      should(utils.dig({a: 1}, 'a', 'b') === void 0).ok();
+    });
+  });
 });
