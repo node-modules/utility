7a5c8fe16f2ae6e135a80a273b3845dafc14b8b4
fengmk2 <m@fengmk2.com>
2016-05-09 12:29:24 +0800

    feat(array): impl faster splice one element on array (#24)
    https://github.com/DavidCai1993/my-blog/issues/27

--- test/array.test.js
+++ test/array.test.js
@@ -26,3 +26,24 @@ test('randomSlice() should return sub items', t => {
   t.deepEqual(utils.randomSlice(arr, 0), arr);
   t.is(utils.randomSlice(arr, 6).length, 6);
 });
+
+test('spliceOne() should work', t => {
+  t.deepEqual(utils.spliceOne([1, 2, 3], 0), [2, 3]);
+  t.deepEqual(utils.spliceOne([1, 2, 3], 1), [1, 3]);
+  t.deepEqual(utils.spliceOne([1, 2, 3], 2), [1, 2]);
+  t.deepEqual(utils.spliceOne([1, 2, 3], 3), [1, 2, 3]);
+  t.deepEqual(utils.spliceOne([1, 2, 3], 4), [1, 2, 3]);
+  t.deepEqual(utils.spliceOne([1, 2, 3], 5), [1, 2, 3]);
+  t.deepEqual(utils.spliceOne([1, 2, 3], -0), [2, 3]);
+  t.deepEqual(utils.spliceOne([1, 2, 3], -1), [1, 2]);
+  t.deepEqual(utils.spliceOne([1, 2, 3], -2), [1, 3]);
+  t.deepEqual(utils.spliceOne([1, 2, 3], -3), [2, 3]);
+  t.deepEqual(utils.spliceOne([1, 2, 3], -4), [1, 2, 3]);
+  t.deepEqual(utils.spliceOne([1, 2, 3], -5), [1, 2, 3]);
+  t.deepEqual(utils.spliceOne([1, 2, 3], 100), [1, 2, 3]);
+
+  t.deepEqual(utils.spliceOne([1], 0), []);
+  t.deepEqual(utils.spliceOne([1], 1), [1]);
+  t.deepEqual(utils.spliceOne([], 0), []);
+  t.deepEqual(utils.spliceOne([], 100), []);
+});
