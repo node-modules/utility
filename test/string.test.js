4a939fc3e7e35f23457f083e429c85fe70af7874
leoner <kangpangpang@gmail.com>
2017-02-13 22:52:45 +0800

    feat: add replace invalid http header character

--- test/string.test.js
+++ test/string.test.js
@@ -64,3 +64,36 @@ test('replace() should support regex', t => {
 
   t.is(utils.replace('{ <body> }', /<body>/, 'this is body $& $` $\' $$'), '{ this is body $& $` $\' $$ }');
 });
+
+test('replaceInvalidHttpHeaderChar() should replace invalid char', t => {
+  var s0 = '';
+  var s1 = '123';
+  var s2 = 'abc';
+  var s3 = '!@#$%^&*()_+-=\|';
+  var s4 = '你1好0';
+  var s5 = '1你1好0';
+  var s6 = '11你1好0';
+  var s7 = '111你1好0';
+  var s8 = '1111你1好0';
+
+  t.is(utils.replaceInvalidHttpHeaderChar(s0).val, s0);
+  t.is(utils.replaceInvalidHttpHeaderChar(s0).invalid, false);
+  t.is(utils.replaceInvalidHttpHeaderChar(s1).val, s1);
+  t.is(utils.replaceInvalidHttpHeaderChar(s1).invalid, false);
+  t.is(utils.replaceInvalidHttpHeaderChar(s2).val, s2);
+  t.is(utils.replaceInvalidHttpHeaderChar(s2).invalid, false);
+  t.is(utils.replaceInvalidHttpHeaderChar(s3).val, s3);
+  t.is(utils.replaceInvalidHttpHeaderChar(s3).invalid, false);
+  t.is(utils.replaceInvalidHttpHeaderChar(s4).val, ' 1 0');
+  t.is(utils.replaceInvalidHttpHeaderChar(s4).invalid, true);
+  t.is(utils.replaceInvalidHttpHeaderChar(s5).val, '1 1 0');
+  t.is(utils.replaceInvalidHttpHeaderChar(s5).invalid, true);
+  t.is(utils.replaceInvalidHttpHeaderChar(s6).val, '11 1 0');
+  t.is(utils.replaceInvalidHttpHeaderChar(s6).invalid, true);
+  t.is(utils.replaceInvalidHttpHeaderChar(s7).val, '111 1 0');
+  t.is(utils.replaceInvalidHttpHeaderChar(s7).invalid, true);
+  t.is(utils.replaceInvalidHttpHeaderChar(s8).val, '1111 1 0');
+  t.is(utils.replaceInvalidHttpHeaderChar(s8).invalid, true);
+  t.is(utils.replaceInvalidHttpHeaderChar(s8, '-').val, '1111-1-0');
+  t.is(utils.replaceInvalidHttpHeaderChar(s8, '-').invalid, true);
+});
