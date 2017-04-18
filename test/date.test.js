f83b6b35637452d3cab162b614ea65420b05381c
fengmk2 <fengmk2@gmail.com>
2017-04-19 02:08:35 +0800

    chore: remove unused comments

--- test/date.test.js
+++ test/date.test.js
@@ -1,16 +1,4 @@
-/**
- * Copyright(c) node-modules and other contributors.
- * MIT Licensed
- *
- * Authors:
- *   fengmk2 <fengmk2@gmail.com> (http://fengmk2.com)
- */
-
 'use strict';
-
-/**
- * Module dependencies.
- */
 
 import moment from 'moment';
 import test from 'ava';
