f1bc2290a54d709dce393ffed8fd71ca731c01fd
yang <1766083035@qq.com>
2018-12-11 08:55:03 +0800

    :sparkles: add defines for the object mothods

--- index.d.ts
+++ index.d.ts
@@ -300,4 +300,35 @@ declare namespace utility {
     lower: number,
     upper: number,
   ): number;
+
+
+  // ** Defines For Object Methods **
+
+  /**
+   * High performance assign before node6
+   * @param {Object} target - target object
+   * @param {Object | Array} objects - object assign from
+   * @return {Object} - return target object
+   */
+  function assign(
+    target: object,
+    objects: object | any[],
+  ): object;
+
+  /**
+   * Module dependencies.
+   */
+  function has(
+    obj: object,
+    props: string,
+  ): boolean;
+
+  /**
+   * generate a real map object(clean object), no constructor, no __proto__
+   * @param {Object} [obj] - init object, optional
+   * @return {Object}
+   */
+  function map(
+    obj?: object,
+  ): object;
 }
